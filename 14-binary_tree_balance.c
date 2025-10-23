39a46759651a2a1c39d95a0b020f756f3dc45f80
root <root@ip-10-42-172-176.ec2.internal>
2025-10-23 08:31:24 +0000

    fix: Correct output - case: Left-height = 2 and right-height = 0

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,24 +1,17 @@
 #include "binary_trees.h"
 
-/**
- * binary_tree_height - Measures the height of a binary tree
- * @tree: Pointer to the root node of the tree to measure the height
- *
- * Return: Height of the tree, or 0 if tree is NULL
- */
-size_t binary_tree_height(const binary_tree_t *tree)
+/* Local helper that measures height in LEVELS (leaf = 1) */
+static size_t height_levels(const binary_tree_t *tree)
 {
-	size_t left_h = 0, right_h = 0;
+	size_t lh, rh;
 
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left)
-		left_h = 1 + binary_tree_height(tree->left);
-	if (tree->right)
-		right_h = 1 + binary_tree_height(tree->right);
+	lh = height_levels(tree->left);
+	rh = height_levels(tree->right);
 
-	return (left_h > right_h ? left_h : right_h);
+	return ((lh > rh ? lh : rh) + 1);
 }
 
 /**
@@ -32,6 +25,6 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	return ((int)binary_tree_height(tree->left) -
-		(int)binary_tree_height(tree->right));
+	return ((int)height_levels(tree->left) -
+		(int)height_levels(tree->right));
 }
